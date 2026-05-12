cba102d488cbd42f950c2c956e8e2452d869f485
Bharath Kumar Bellam <82457244+Bellambharath@users.noreply.github.com>
2026-05-12 12:02:55 +0530

    fix: delete junk CompanyMembershipEntity.cs (invalid partial on sealed class)

--- backend/CommutePool.Infrastructure/Persistence/Entities/CompanyMembershipEntity.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace CommutePool.Infrastructure.Persistence.Entities;
-
-// Navigation fix: add Company navigation to UserCompanyMembershipEntity
-public sealed partial class UserCompanyMembershipEntityExtensions { }
-
-// Patch: extend UserCompanyMembershipEntity with navigation (EF will pick this up)
-// We add a partial class in same namespace to add navigation property
